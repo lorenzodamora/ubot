c6fe4482c97ae7d9b3c725006af7df6685577ce7
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2024-02-10 15:24:32 +0100

    rename check_output

--- start_check.sh
+++ start_check.sh
@@ -2,5 +2,5 @@
 project_path="/home/ubuntu/Magnus/PycharmProj/ubot/"
 cd "${project_path}"
 source "${project_path}/.python_venv/bin/activate"
-python -u check_output.py &
+python -u check_output_1.py &
 deactivate
--- start_check2.sh
+++ start_check2.sh
@@ -2,5 +2,5 @@
 project_path="/home/ubuntu/Magnus/PycharmProj/ubot2/"
 cd "${project_path}"
 source "${project_path}/.python_venv/bin/activate"
-python -u check_output.py &
+python -u check_output_2.py &
 deactivate
